08acb440e56f6772dbf8dec0753498b557ae0bce
Appoline Cogan <appolinecogan@gmail.com>
2022-10-20 11:33:16 +1100

    added navbas styles and image

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,45 +1,75 @@
 import React from "react";
+import appoline from "../Appoline.png"
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="">
-      <li className="">
-        <a
-          href="#home"
-          onClick={() => handlePageChange("Home")}
-          className={currentPage === "Home" ? "nav-link active" : "nav-link"}
-        >
-          Home
+    <nav className="navbar navbar-expand-lg navbar-light sticky-top">
+      <div className="container">
+        <a className="navbar-brand" href="#home">
+          <img src={appoline} alt="" height="80" />
+          <span className="m-4">Appoline Cogan</span>
         </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange("About")}
-          className={currentPage === "About" ? "nav-link active" : "nav-link"}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#blog"
-          onClick={() => handlePageChange("Blog")}
-          className={currentPage === "Blog" ? "nav-link active" : "nav-link"}
-        >
-          Blog
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange("Contact")}
-          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a
+                href="#home"
+                onClick={() => handlePageChange("Home")}
+                className={
+                  currentPage === "Home" ? "nav-link active" : "nav-link"
+                }
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#about"
+                onClick={() => handlePageChange("About")}
+                className={
+                  currentPage === "About" ? "nav-link active" : "nav-link"
+                }
+              >
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#blog"
+                onClick={() => handlePageChange("Blog")}
+                className={
+                  currentPage === "Blog" ? "nav-link active" : "nav-link"
+                }
+              >
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#contact"
+                onClick={() => handlePageChange("Contact")}
+                className={
+                  currentPage === "Contact" ? "nav-link active" : "nav-link"
+                }
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
