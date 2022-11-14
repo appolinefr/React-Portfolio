51fbb59cd7fb6b5d8354e88d7e398f72020251d7
Appoline Cogan <appolinecogan@gmail.com>
2022-11-14 17:03:51 +1100

    improved styles

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,49 +2,27 @@ import React from "react";
 import appoline from "../../images/AppolineNav.png";
 import { Nav, Navbar } from "react-bootstrap";
 
-function NavTabs({ currentPage, handlePageChange }) {
+function NavTabs() {
   return (
     <Navbar collapseOnSelect expand="lg p-3">
       <Navbar.Brand href="/">
-        <img
-          src={appoline}
-          alt="Appoline Cogan"
-          height="60"
-          onClick={() => handlePageChange("/about")}
-        />
+        <img src={appoline} alt="Appoline Cogan" height="60" href="/" />
         <span className="m-4 fs-3">AC</span>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ms-auto fs-4 mx-2">
           <Nav.Link
-            href="/"
-            className={currentPage === "About" ? "nav-link active" : "nav-link"}
-          >
+            href="/">
             About
           </Nav.Link>
           <Nav.Link
             href="/projects"
-            className={currentPage === "Projects" ? "nav-link active" : "nav-link"}
           >
             Projects
           </Nav.Link>
-          <Nav.Link
-            href="/contact"
-            className={
-              currentPage === "Contact" ? "nav-link active" : "nav-link"
-            }
-          >
-            Contact
-          </Nav.Link>
-          <Nav.Link
-            href="/resume"
-            className={
-              currentPage === "Resume" ? "nav-link active" : "nav-link"
-            }
-          >
-            Resume
-          </Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+          <Nav.Link href="/resume">Resume</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
